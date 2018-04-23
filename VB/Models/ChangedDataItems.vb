c8877e7c6607040dd865a6b87fc83120d9c46f67
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Models/ChangedDataItems.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+
+Namespace Q329588.Models
+	Public Class ChangedDataItems
+		Private privatekey As Int32
+		Public Property key() As Int32
+			Get
+				Return privatekey
+			End Get
+			Set(ByVal value As Int32)
+				privatekey = value
+			End Set
+		End Property
+		Private privatefield As String
+		Public Property field() As String
+			Get
+				Return privatefield
+			End Get
+			Set(ByVal value As String)
+				privatefield = value
+			End Set
+		End Property
+		Private privatevalue As String
+		Public Property value() As String
+			Get
+				Return privatevalue
+			End Get
+			Set(ByVal value As String)
+				privatevalue = value
+			End Set
+		End Property
+	End Class
+End Namespace
---
+++ VB/Views/Home/Index.vbhtml
@@ -0,0 +1,15 @@
+﻿@Code
+	ViewBag.Title = "How to perform GridView batch updating using different editors in the DataItem template "
+End Code
+<h2>@ViewBag.Message</h2>
+<div style="width: 400px">
+	@Html.Action("GridViewPartial")
+	@Html.DevExpress().Button(Sub(s)
+							   s.Name = "customButton"
+							   s.UseSubmitBehavior = False
+							   s.ClientSideEvents.Click = String.Format("function (s, e) {{ OnClick('{0}'); }}", Url.Action("UpdateValue", "Home", Nothing))
+						   End Sub).GetHtml()
+	<div class="status">
+		AJAX status
+	</div>
+</div>
